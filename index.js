a3afbe4cc541fd1db7af9f217218508049bf38b4
Ansh Agrawal <72245838+CH1NRU5T@users.noreply.github.com>
2021-10-12 08:05:46 +0000

    fully working

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const Discord = require(`discord.js`);
-var rp = require('request-promise');
+var hastebin = require('hastebin')
 const client = new Discord.Client();
-const axios = require('axios');
-var textToPaste="";
+const keepAlive = require('./server')
 
-const prefix = ":"
-function createParams(text1){
-const params = new URLSearchParams()
-params.append('api_dev_key', process.env.KEY)
-params.append('api_paste_code', text1)
-params.append('api_option', 'paste')
-return params;
-}
-
+const prefix = "."
 
 client.on(`ready`, ()=>{
   console.log(`Logged in as ${client.user.tag}!`)
@@ -23,38 +14,25 @@ client.on(`message`, async msg=>{
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
   const args = msg.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
-  if(command === `ping`){
+  if(command === `paste`){
     pasteToPastebinAndReturnLink(msg, msg.content.slice(prefix.length + command.length+1));
     // console.log(msg.content.slice(prefix.length + command.length+1))
   }
 });
 
-function argsToText(args){
-  var text2=""
-  args.forEach(element=>{
-    text2+=element;
-  })
-  return text2;
-}
 
-const config = {
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-}
 
 function pasteToPastebinAndReturnLink(msg,text){
-axios.post('https://pastebin.com/api/api_post.php', createParams(text), config)
-  .then((result) => {
-    // console.log(result.data)
-    // return result.data;
-    msg.reply(result.data)
-  })
-  .catch((err) => {
-    // console.log(err)
-    // return err;
-    msg.reply(err)
-  })
+hastebin.createPaste(text,{
+  raw:false,
+  contentType:'text/plain',
+  server:'https://hastebin.com'
+}, /*options for the got module here*/{})
+    .then((url)=>{
+      msg.reply(url)
+    }).catch((err)=>
+    msg.reply(err))
 }
 
+keepAlive()
 client.login(process.env.TOKEN);
